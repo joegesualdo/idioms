7eab66c648537a65c3416cee318931ef6892a61c
Joe Gesualdo <joegesualdo@gmail.com>
2015-09-30 17:19:47 -0400

    Inital commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Idioms 
+> List of idioms 
+
+## Install
+```
+$ npm install --save idioms 
+```
+
+## Usage
+```javascript
+var Idioms = require('idioms')
+
+Idioms.getIdioms(function(idioms){
+  console.log(idioms)
+})
+```
+
+## API
+### .getIdioms(callback)
+```javascript
+var Idioms = require('idioms')
+
+Idioms.getIdioms(function(idioms){
+  console.log(idioms)
+})
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+var path = require('path')
+
+var Idioms = {
+  getIdioms: getIdioms
+}
+
+function getIdioms(callback){
+  idioms = []
+  var rl = require('readline').createInterface({
+    input: require('fs').createReadStream(__dirname + '/idioms.txt')
+  });
+
+  rl.on('line', function(line) {
+    idioms.push(line)
+  });
+  rl.on('close', function(){
+    callback(idioms)
+  })
+}
+
+module.exports = Idioms
+
+Idioms.getIdioms(function(idioms){
+  console.log(idioms)
+})
