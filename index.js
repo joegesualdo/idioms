11a57e317486eca07465da32e88987d29c8c9ad7
Joe Gesualdo <joegesualdo@gmail.com>
2015-10-02 09:00:59 -0400

    Add idioms from Wiki

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var path = require('path')
+var readline = require('readline')
 
 var Idioms = {
   getIdioms: getIdioms
@@ -7,15 +8,39 @@ var Idioms = {
 
 function getIdioms(callback){
   idioms = []
-  var rl = require('readline').createInterface({
-    input: require('fs').createReadStream(__dirname + '/idioms.txt')
+  getFreeDictionaryIdioms(function(freeDictionaryIdioms){
+    idioms = idioms.concat(freeDictionaryIdioms)
+    getWikiIdioms(function(wikiIdioms){
+      idioms = idioms.concat(wikiIdioms)
+      callback(idioms)
+    })
+  })
+}
+function getFreeDictionaryIdioms(callback){
+  var freeDictionaryIdiomsList  = []
+  var rl = readline.createInterface({
+    input: fs.createReadStream(__dirname + '/free-dicionary-idioms.txt')
   });
 
   rl.on('line', function(line) {
-    idioms.push(line)
+    freeDictionaryIdiomsList.push(line)
   });
   rl.on('close', function(){
-    callback(idioms)
+    callback(freeDictionaryIdiomsList)
+  })
+}
+
+function getWikiIdioms(callback){
+  var wikiIdiomList = []
+  var rl = readline.createInterface({
+    input: fs.createReadStream(__dirname + '/wiki-idioms.txt')
+  });
+
+  rl.on('line', function(line) {
+    wikiIdiomList.push(line)
+  });
+  rl.on('close', function(){
+    callback(wikiIdiomList)
   })
 }
 
